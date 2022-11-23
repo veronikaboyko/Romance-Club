de24b58f00d224f06ff7a9e19ef62c6bfb69634a
necuteboy <89706104+necuteboy@users.noreply.github.com>
2022-11-24 00:47:06 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
-package org.example;
+papackage org.example;
 
+
+import org.example.model.User;
+import org.example.telegram.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        User user = new User();
-        user.start();
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        TelegramBot bot = new TelegramBot();
+        botsApi.registerBot(bot);
     }
 }
