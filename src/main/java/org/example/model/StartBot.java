0587d054bc6c8604fc406291b51797955d2ea243
necuteboy <89706104+necuteboy@users.noreply.github.com>
2022-12-18 18:31:40 +0500

    Add files via upload

---
+++ src/main/java/org/example/model/StartBot.java
@@ -0,0 +1,28 @@
+package org.example.model;
+import org.example.telegram.TelegramBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class StartBot {
+    public static String getFromProperty(String fileName, String key) {
+        Properties properties = new Properties();
+        try (FileInputStream fis =
+                     new FileInputStream("src/main/resources/" + fileName)) {
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("Конфигурационный файл отсутствует!", e);
+        }
+
+        return properties.getProperty(key);
+    }
+    public static void startTgBot() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        TelegramBot bot = new TelegramBot(getFromProperty("config.properties", "token"), "Tutorial bot");
+        botsApi.registerBot(bot);
+    }
+}
