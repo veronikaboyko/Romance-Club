56d6d3b517e939c4c0d832a9f57bc4e5e9f8b01e
necuteboy <89706104+necuteboy@users.noreply.github.com>
2022-12-18 18:32:18 +0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,10 @@
 package org.example;
 
-import org.example.telegram.TelegramBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.example.model.StartBot;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-        startTgBot();
-    }
-
-    public static String getFromProperty(String fileName, String key) {
-        Properties property = new Properties();
-        try (FileInputStream fis =
-                     new FileInputStream("src/main/resources/" + fileName)) {
-            property.load(fis);
-        } catch (IOException e) {
-            throw new RuntimeException("Конфигурационный файл отсутствует!", e);
-        }
-
-        return property.getProperty(key);
-    }
-    public static String getPhrase(String fileName, String key, String language, String country) {
-        Locale locale = new Locale(language, country);
-        ResourceBundle rb = ResourceBundle.getBundle(fileName, locale);
-
-        return rb.getString(key);
-    }
-    private static void startTgBot() throws TelegramApiException {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        TelegramBot bot = new TelegramBot(Main.getFromProperty("config.properties", "token"), "Tutorial bot");
-        botsApi.registerBot(bot);
+        StartBot.startTgBot();
     }
 }
