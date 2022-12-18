93f8fab3431be21aa821c0a3813846db0f379d35
necuteboy <89706104+necuteboy@users.noreply.github.com>
2022-12-18 18:35:39 +0500

    Update Handler.java

--- src/main/java/org/example/telegram/Handler.java
+++ src/main/java/org/example/telegram/Handler.java
@@ -2,13 +2,53 @@
 package org.example.telegram;
 import org.example.model.Episode;
 import org.example.model.Story;
-import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 public class Handler {
+    /**
+     * проверка на существование следущего абзаца в истории
+     * @param what - то что вводит пользователь
+     * @param length - длина всей истории
+     * @param count - номер абзаца в истории
+     * @return  условие существования
+     */
+    private boolean conditionToNext(String what , int length, int count){
+        return (what.equals("next") && count < length - 1);
+    }
+
+    /**
+     * проверка на существование предыдущего абзаца в истории
+     * @param what - то что вводит пользователь
+     * @param count - номер абзаца в истории
+     * @return условие существования
+     */
+    private boolean conditionToBefore(String what , int count){
+        return (what.equals("before") && count > 1);
+    }
+
+    /**
+     *  проверка достижения начала истории
+     * @param count - номер абзаца в истории
+     * @return возвращает true если достигли начала
+     */
+    private boolean conditionToStart(int count){
+        return count == 1;
+    }
+
+    /**
+     *  проверка на достижение конца истории
+     * @param what - то что вводит пользователь
+     * @param what - то что вводит пользователь
+     * @param length - длина всей истории
+     * @param count - номер абзаца в истории
+     * @return условие достижения конца истории
+     */
+    private boolean conditionToEnd(String what , int count, int length){
+        return count == length -1 && what.equals("next");
+    }
     /**
      * Метод Restart возвращает в начало работы гайда
      */
@@ -55,23 +95,17 @@ public class Handler {
      * @return абзац истории
      */
     public SendMessage Story (Long who, String what , String list, int count){
-        System.out.println(count);
         String[] splitList = list.split("\n");
-        System.out.println(splitList.length);
         SendMessage test = new SendMessage();
         test.setChatId(who);
-        if (what.equals("next") && count < splitList.length - 1) {
+        if (conditionToNext(what,splitList.length,count)) {
             test.setText((splitList[count + 1 ]));
-            return test;
-        } else if (what.equals("before") && count > 1) {
+        } else if (conditionToBefore(what,count)) {
             test.setText((splitList[count - 1]));
-            return test;
-        }else if (count == 1) {
+        }else if (conditionToStart(count)) {
             test.setText(("Начало гайда:"));
-            return test;
-        } else if (count >= splitList.length - 1 && what.equals("next")) {
+        } else if (conditionToEnd(what , count,splitList.length)) {
             test.setText(("Конец гайда:"));
-            return test;
         }
         return test;
     }
