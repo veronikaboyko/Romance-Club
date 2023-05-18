1c2af65e86114114be499302c8447cd900a379e4
necuteboy <89706104+necuteboy@users.noreply.github.com>
2023-05-18 12:39:39 +0500

    Delete Page.java

--- src/main/java/org/example/Page.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public interface Page {
-    /**
-     * @param link страница
-     * @return page возвращает страницу в виде строки
-     */
-    String getPage (String link);
-}
